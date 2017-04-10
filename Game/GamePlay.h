80c6dd569b266d09f7565c162c44f6785f5679c0
ngspmayx <ngspmayxdabadaba@gmail.com>
2017-04-10 10:01:18 +0900

    Foodの調整 Waveの追加

--- Game/GamePlay.h
+++ Game/GamePlay.h
@@ -16,6 +16,7 @@ class Food;
 
 //定数
 const int FOOD_NUM = 4;		//画面内に1度に出てくる食材の数
+const int BREAD_NUM = 5;	//パンの残り枚数
 
 //クラス
 class Play:public GameBase
@@ -23,9 +24,13 @@ class Play:public GameBase
 private:
 	Player** m_bread;		// 食パン
 	Food* m_food[FOOD_NUM];
+	int m_bread_num;		//パンの残り枚数
+	bool m_wave_clear;		//1WAVEが終わったかどうかの判定
 	
 	void InitBread();		// パン初期化
 	void ReleaseBread();	// パン開放
+	void FoodAwake();		// 食材の出現
+	void UpdateWave();		// WAVEの更新処理
 
 public:
 	Play();
